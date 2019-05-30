daeb5eeaedbb60464b596e1a5fa1e65a3eaef86c
Philip Daniels <Philip.Daniels1971@gmail.com>
2019-05-30 22:59:48 +0100

    Update documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,110 @@
+//! Provides a couple of simple timers that output messages indicating the elapsed time via the
+//! [log](https://crates.io/crates/log) crate when they are dropped.
+//!
+//! Timers have names, and the log messages are constructed in such a way that they contain
+//! the module, filename and line number of the place where the timer was constructed.
+//!
+//! Timers are usually created using the `timer!` or `stimer!` macros. The difference is
+//! that `timer!` returns a timer that logs a message only when it is dropped, while `stimer!`
+//! returns a timer that logs a started message as soon as it is created, and a finished
+//! message when it is dropped.
+//!
+//! Example - "Find Files" is the name of the timer:
+//!
+//! ```norun
+//! use logging_timer::{timer};
+//!
+//! fn find_files(dir: PathBuf) -> List<PathBuf> {
+//!     let _tmr = timer!("Find Files");
+//!     let files = vec![];
+//!
+//!     // expensive operation here
+//!
+//!     return files;
+//! } // _tmr is dropped here and a 'TimerFinished' message is logged
+//!```
+//!
+//! You can replace `timer!` with `stimer!` to get a timer that logs a starting message as
+//! well, giving you a pair of 'bracketing' log messages.
+//!
+//! In addition, both timer macros accept [format_args!](https://doc.rust-lang.org/std/macro.format_args.html)
+//! style parameters, allowing you to include extra information in the log messages.
+//!
+//! ```norun
+//! let tmr = timer!("Find Files", "Directory = {}", dir);
+//! ```
+//!
+//! # Outputting Intermediate Messages
+//!
+//! The `executing!` macro allows you to make the timer produce a message before it is dropped.
+//! You can call it as many times as you want. A pseudocode example:
+//!
+//! ```norun
+//! use logging_timer::{timer, executing};
+//!
+//! fn find_files(dir: PathBuf) -> List<PathBuf> {
+//!     let tmr = timer!("Find Files");
+//!     let files = vec![];
+//!
+//!     for dir in sub_dirs(dir) {
+//!         // expensive operation
+//!         executing!(tmr, "Processed {}", dir);
+//!     }
+//!
+//!     return files;
+//! } // tmr is dropped here and a 'TimerFinished' message is logged
+//!```
+//!
+//! # Controlling the Final Message
+//!
+//! Finally, the `finish!` macro also makes the timer log a message, but it also has the side
+//! effect of suppressing the normal drop message.  `finish!` is useful when you want the final
+//! message to include some information that you did not have access to until the calculation had
+//! finished.
+//!
+//! ```norun
+//! use logging_timer::{timer, executing, finish};
+//!
+//! fn find_files(dir: PathBuf) -> List<PathBuf> {
+//!     let tmr = timer!("Find Files");
+//!     let files = vec![];
+//!
+//!     finish!(tmr, "Found {} files", files.len());
+//!     return files;
+//! } // tmr is dropped here but no message is produced.
+//!```
+//!
+//! # Example of Timer Output
+//!
+//! The overall format will depend on how you customize the output format of the log crate, but as an illustrative example:
+//!
+//! ```text
+//! 2019-05-30T21:41:41.847982550Z DEBUG [TimerStarting] [dnscan/src/main.rs/63] Directory Analysis
+//! 2019-05-30T21:41:41.868690703Z INFO [dnlib::configuration] [dnlib/src/configuration.rs/116] Loaded configuration from "/home/phil/.dnscan/.dnscan.json"
+//! 2019-05-30T21:41:41.897609281Z DEBUG [TimerFinished] [dnlib/src/io.rs/67] Elapsed=28.835275ms, Find Files, Dir="/home/phil/mydotnetprojects", NumSolutions=1 NumCsproj=45, NumOtherFiles=12
+//! 2019-05-30T21:41:41.955140835Z DEBUG [TimerFinished] [dnlib/src/analysis.rs/93] Elapsed=57.451736ms, Load And Analyze Solution files
+//! 2019-05-30T21:41:42.136762196Z DEBUG [TimerFinished] [dnlib/src/analysis.rs/108] Elapsed=181.563223ms, Load And Analyze Project files, Found 43 linked projects and 2 orphaned projects
+//! 2019-05-30T21:41:42.136998556Z DEBUG [TimerStarting] [dnscan/src/main.rs/87] Calculate project graphs and redundant projects
+//! 2019-05-30T21:41:42.143072972Z DEBUG [TimerExecuting] [dnscan/src/main.rs/87] Elapsed=6.075205ms, Calculate project graphs and redundant projects, Individual graphs done
+//! 2019-05-30T21:41:42.149218039Z DEBUG [TimerFinished] [dnscan/src/main.rs/87] Elapsed=12.219438ms, Calculate project graphs and redundant projects, Found 19 redundant project relationships
+//! 2019-05-30T21:41:42.165724712Z DEBUG [TimerFinished] [dnscan/src/main.rs/108] Elapsed=16.459312ms, Write output files
+//! 2019-05-30T21:41:42.166445Z INFO [TimerFinished] [dnscan/src/main.rs/63] Elapsed=318.48581ms, Directory Analysis
+//! ```
+//!
+//! Here the `[Timer*]` blocks are the `target` field from log's [Record](https://docs.rs/log/0.4.6/log/struct.Record.html)
+//! struct and `[dnscan/src/main.rs/63]` is the filename and number from `Record` - this captures the place where the timer was
+//! instantiated. The module is also set, but is not shown in these examples.
+//! Note how the timer named 'Directory Analysis' is the only one to log both a starting and ending message.
+//!
+//! By default messages are output at DEBUG level, you can control this using the `level()` builder method:
+//!
+//! ```norun
+//! let _tmr = timer!("Directory Analysis").level(Level::Info);
+//! ```
+//!
+//! (There is a bug, this does not work for `stimer!` yet, only `timer!`)
+
+
 use log::{log_enabled, Level, RecordBuilder};
 use std::fmt;
 use std::sync::atomic::{AtomicBool, Ordering};
@@ -5,7 +112,7 @@ use std::time::Instant;
 
 /// When this struct is dropped, it logs a message stating its name and how long
 /// the execution time was. Can be used to time functions or other critical areas.
-pub struct LoggingTimer<'a> {
+pub struct LoggingTimer<'name> {
     /// The log level. Defaults to Debug.
     level: Level,
     /// Set by the file!() macro to the name of the file where the timer is instantiated.
@@ -20,21 +127,21 @@ pub struct LoggingTimer<'a> {
     /// The instant, in UTC, that the timer was instantiated.
     start_time: Instant,
     /// The name of the timer. Used in messages to identify it.
-    name: &'a str,
+    name: &'name str,
     /// Any extra information to be logged along with the name. Unfortunately, due
     /// to the lifetimes associated with a `format_args!` invocation, this currently allocates
     /// if you use it.
     extra_info: Option<String>,
 }
 
-impl<'a> LoggingTimer<'a> {
-    /// Constructs a new `LoggingTimer` that prints only a 'Finished' message.
+impl<'name> LoggingTimer<'name> {
+    /// Constructs a new `LoggingTimer` that prints only a 'TimerFinished' message.
     /// This method is not usually called directly, use the `timer!` macro instead.
     pub fn new(
         file: &'static str,
         module_path: &'static str,
         line: u32,
-        name: &'a str,
+        name: &'name str,
         extra_info: Option<String>,
     ) -> Self
     {
@@ -50,13 +157,13 @@ impl<'a> LoggingTimer<'a> {
         }
     }
 
-    /// Constructs a new `LoggingTimer` that prints a 'Starting' and a 'Finished' message.
+    /// Constructs a new `LoggingTimer` that prints a 'TimerStarting' and a 'TimerFinished' message.
     /// This method is not usually called directly, use the `stimer!` macro instead.
     pub fn with_start_message(
         file: &'static str,
         module_path: &'static str,
         line: u32,
-        name: &'a str,
+        name: &'name str,
         extra_info: Option<String>,
     ) -> Self
     {
@@ -86,24 +193,28 @@ impl<'a> LoggingTimer<'a> {
     }
 
     /// Sets the logging level.
+    /// Note that this consumes self, so that it can be called in a one-liner like this:
+    ///
+    /// ```norun
+    /// let tmr = timer!("foo").level(Level::Trace);
+    /// ```
     pub fn level(mut self, level: Level) -> Self {
         self.level = level;
         self
     }
 
-    /// Outputs a log message showing the current elapsed time, but does not stop the timer.
-    /// This method can be called multiple times until the timer is dropped.
+    /// Outputs a log message with a target of 'TimerExecuting' showing the current elapsed time, but does not
+    /// stop the timer. This method can be called multiple times.
     /// The message can include further information via a `format_args!` approach.
-    /// This method is usually not called directly, it is easier to call via the `progress!`
-    /// macro.
-    pub fn progress(&self, args: Option<fmt::Arguments>) {
+    /// This method is usually not called directly, it is easier to use the `executing!` macro.
+    pub fn executing(&self, args: Option<fmt::Arguments>) {
         self.log_impl(TimerTarget::Executing, args);
     }
 
-    /// Outputs a 'Finished' log message and suppresses the normal message that is
-    /// output when the timer is dropped. The message can include further `format_args!`
+    /// Outputs a log message with a target of 'TimerFinished' and suppresses the normal message
+    /// that is output when the timer is dropped. The message can include further `format_args!`
     /// information. This method is normally called using the `finish!` macro. Calling
-    /// finish() again will have no effect.
+    /// `finish()` again will have no effect.
     pub fn finish(&self, args: Option<fmt::Arguments>) {
         if !self.finished.load(Ordering::SeqCst) {
             self.finished.store(true, Ordering::SeqCst);
@@ -146,7 +257,8 @@ impl<'a> LoggingTimer<'a> {
 }
 
 impl<'a> Drop for LoggingTimer<'a> {
-    /// Drops the timer, outputting a 'Finished' message if `finish` has not yet been called.
+    /// Drops the timer, outputting a log message with a target of `TimerFinished`
+    /// if the `finish` method has not yet been called.
     fn drop(&mut self) {
         self.finish(None);
     }
@@ -239,6 +351,26 @@ macro_rules! stimer {
     };
 }
 
+/// Makes an existing timer output an 'executing' mesasge.
+/// Can be called multiple times.
+#[macro_export]
+macro_rules! executing {
+    ($timer:expr) => ({
+        $timer.executing(None)
+    });
+
+    ($timer:expr, $format:tt) => ({
+        $timer.executing(Some(format_args!($format)))
+    });
+
+    ($timer:expr, $format:tt, $($arg:expr),*) => ({
+        $timer.executing(Some(format_args!($format, $($arg), *)))
+    })
+}
+
+/// Makes an existing timer output a 'finished' mesasge and suppresses
+/// the normal drop message.
+/// Only the first call has any effect, subsequent calls will be ignored.
 #[macro_export]
 macro_rules! finish {
     ($timer:expr) => ({
@@ -254,17 +386,44 @@ macro_rules! finish {
     })
 }
 
-#[macro_export]
-macro_rules! progress {
-    ($timer:expr) => ({
-        $timer.progress(None)
-    });
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use log::Level;
 
-    ($timer:expr, $format:tt) => ({
-        $timer.progress(Some(format_args!($format)))
-    });
+    // Just some simple tests to check that the macros still generate
+    // valid code when refactoring.
 
-    ($timer:expr, $format:tt, $($arg:expr),*) => ({
-        $timer.progress(Some(format_args!($format, $($arg), *)))
-    })
+    #[test]
+    pub fn construction() {
+        let _ = timer!("foo");
+        let _ = timer!("foo", "pat");
+        let _ = timer!("foo", "pat {}", 1);
+
+        let _ = stimer!("foo");
+        let _ = stimer!("foo", "pat");
+        let _ = stimer!("foo", "pat {}", 1);
+    }
+
+    #[test]
+    pub fn executing() {
+        let tmr = timer!("foo");
+        executing!(tmr);
+        executing!(tmr, "pat");
+        executing!(tmr, "pat {}", 1);
+    }
+
+    #[test]
+    pub fn finish() {
+        let tmr = timer!("foo");
+        finish!(tmr);
+        finish!(tmr, "pat");
+        finish!(tmr, "pat {}", 1);
+    }
+
+    #[test]
+    pub fn level() {
+        let tmr = timer!("foo").level(Level::Trace);
+        assert_eq!(tmr.level, Level::Trace);
+    }
 }
